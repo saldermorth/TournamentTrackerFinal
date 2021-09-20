761c76ecbe940489517e80424b1386f498800fee
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-09-20 10:52:06 +0200

    Update TextConnector.cs

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -66,21 +66,6 @@ namespace TrackerLibrary.DataAccess
         public TeamModel CreateTeam(TeamModel model)
         {
             List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
-
-            //Find the max Id
-            int currentId = 1;
-
-            if (teams.Count > 0)
-            {
-                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
-            }
-
-            model.Id = currentId;
-
-            teams.Add(model);
-            teams.SaveToTeamFile(TeamFile);
-
-            return model;
         }
 
         public List<PersonModel> GetPerson_All()
@@ -88,11 +73,6 @@ namespace TrackerLibrary.DataAccess
             return PeopleFile.FullFilePath().LoadFile().ConvertToPersonModels();
 
             
-        }
-
-        public List<TeamModel> GetTeam_All()
-        {
-            throw new NotImplementedException();
         }
     }
 }
